2f5848f96625ddeb50b8892a39192c5f71679d86
AlanGarc1a <alangarcia969@gmail.com>
2022-05-19 20:38:27 -0700

    add openMenu state and sidebar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from '../Navbar';
+import Sidebar from "../Sidebar";
 
 const Layout = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggle = () => {
+        setOpenMenu(!openMenu);
+    }
+
     return (
         <>
-            <Navbar />
+            <Navbar toggle={toggle} />
+            <Sidebar isOpen={openMenu} toggle={toggle} />
             <main>
                 <Outlet />
             </main>
