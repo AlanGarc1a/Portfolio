7d118436dfb2872c528626ff7d3f2b8c95be408e
AlanGarc1a <alangarcia969@gmail.com>
2022-05-20 19:21:58 -0700

    add contact route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GlobalStyle from './globalStyles';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
             <Route index element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
