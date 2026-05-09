633b075fe6eb6a3dc65eef11e73c98eb1a98bb70
Alan Garcia <alangarcia969@gmail.com>
2026-05-09 14:48:15 -0700

    fix: add new changes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,57 +1,57 @@
-import React from 'react'
+import React from "react";
 import {
-    Nav,
-    NavContainer,
-    NavItem,
-    NavItemLink,
-    NavList,
-    NavListItem,
-    NavLogo,
-    NavLogoTitle,
-    NavItemResume,
-    NavAG,
-    NavMenu
-} from './Navbar.styles';
-import Resume from '../../assets/resume.pdf';
+  Nav,
+  NavContainer,
+  NavItem,
+  NavItemLink,
+  NavList,
+  NavListItem,
+  NavLogo,
+  NavLogoTitle,
+  NavItemResume,
+  NavAG,
+  NavMenu,
+} from "./Navbar.styles";
+import Resume from "../../assets/Resume.pdf";
 
 const Navbar = ({ toggle }) => {
+  const navlinks = [
+    { url: "/", title: "Home" },
+    { url: "/about", title: "About" },
+    { url: "/projects", title: "Projects" },
+    { url: "/contact", title: "Contact" },
+  ];
 
-    const navlinks = [
-        { url: '/', title: 'Home' },
-        { url: '/about', title: 'About' },
-        { url: '/projects', title: 'Projects' },
-        { url: '/contact', title: 'Contact' }
-    ];
-
-    return (
-        <NavContainer>
-            <Nav>
-                <NavLogo>
-                    <NavLogoTitle>Alan </NavLogoTitle>
-                    Garcia
-                </NavLogo>
-                <NavAG>
-                    <NavLogoTitle>A</NavLogoTitle>
-                    G
-                </NavAG>
-                <NavItem>
-                    <NavList>
-                        {navlinks.map(link => {
-                            return (
-                                <NavListItem>
-                                    <NavItemLink to={link.url}>{link.title}</NavItemLink>
-                                </NavListItem>
-                            )
-                        })}
-                        <NavListItem>
-                            <NavItemResume href={Resume} target="_blank">Resume</NavItemResume>
-                        </NavListItem>
-                    </NavList>
-                    <NavMenu onClick={toggle} />
-                </NavItem>
-            </Nav>
-        </NavContainer>
-    )
-}
+  return (
+    <NavContainer>
+      <Nav>
+        <NavLogo>
+          <NavLogoTitle>Alan </NavLogoTitle>
+          Garcia
+        </NavLogo>
+        <NavAG>
+          <NavLogoTitle>A</NavLogoTitle>G
+        </NavAG>
+        <NavItem>
+          <NavList>
+            {navlinks.map((link) => {
+              return (
+                <NavListItem>
+                  <NavItemLink to={link.url}>{link.title}</NavItemLink>
+                </NavListItem>
+              );
+            })}
+            <NavListItem>
+              <NavItemResume href={Resume} target="_blank">
+                Resume
+              </NavItemResume>
+            </NavListItem>
+          </NavList>
+          <NavMenu onClick={toggle} />
+        </NavItem>
+      </Nav>
+    </NavContainer>
+  );
+};
 
 export default Navbar;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,45 +1,40 @@
-import React from 'react'
-import { TransparentButton } from '../Button';
+import React from "react";
+import { TransparentButton } from "../Button";
 import {
-    Menu,
-    CloseSidebar,
-    SidebarContent,
-    SideLink,
-    SideMenuInner,
-    SideMenuItem
-} from './Sidebar.styles'
-import Resume from '../../assets/resume.pdf';
+  Menu,
+  CloseSidebar,
+  SidebarContent,
+  SideLink,
+  SideMenuInner,
+  SideMenuItem,
+} from "./Sidebar.styles";
+import Resume from "../../assets/Resume.pdf";
 
 const Sidebar = ({ isOpen, toggle }) => {
+  const navlinks = [
+    { url: "/", title: "Home" },
+    { url: "/about", title: "About" },
+    { url: "/projects", title: "Projects" },
+    { url: "/contact", title: "Contact" },
+  ];
 
-    const navlinks = [
-        { url: '/', title: 'Home' },
-        { url: '/about', title: 'About' },
-        { url: '/projects', title: 'Projects' },
-        { url: '/contact', title: 'Contact' }
-    ];
-
-    return (
-        <SidebarContent isOpen={isOpen}>
-            <Menu>
-                <CloseSidebar onClick={toggle} />
-            </Menu>
-            <SideMenuInner>
-                {
-                    navlinks.map(link => {
-                        return (
-                            <SideMenuItem onClick={toggle}>
-                                <SideLink to={link.url}>
-                                    {link.title}
-                                </SideLink>
-                            </SideMenuItem>
-                        )
-                    })
-                }
-                <TransparentButton href={Resume}>Resume</TransparentButton>
-            </SideMenuInner>
-        </SidebarContent>
-    )
-}
+  return (
+    <SidebarContent isOpen={isOpen}>
+      <Menu>
+        <CloseSidebar onClick={toggle} />
+      </Menu>
+      <SideMenuInner>
+        {navlinks.map((link) => {
+          return (
+            <SideMenuItem onClick={toggle}>
+              <SideLink to={link.url}>{link.title}</SideLink>
+            </SideMenuItem>
+          );
+        })}
+        <TransparentButton href={Resume}>Resume</TransparentButton>
+      </SideMenuInner>
+    </SidebarContent>
+  );
+};
 
 export default Sidebar;
